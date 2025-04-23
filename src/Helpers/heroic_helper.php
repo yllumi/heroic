6e7254c90ab7e1579280af208fc4d531800f5473
Toni Haryanto <toha.samba@gmail.com>
2025-04-23 09:45:08 +0700

    Interpolate now added automatically by detecting parameter in template

--- src/Helpers/heroic_helper.php
+++ src/Helpers/heroic_helper.php
@@ -5,40 +5,38 @@ function renderRouter($router = [], $minify = false)
 {
     $routerString = "";
 
-    foreach($router as $route => $routeProp) 
-    {
+    foreach ($router as $route => $routeProp) {
         $routePath = is_string($routeProp) ? $routeProp : $route;
-        
         $routerString .= "<template \nx-route=\"{$routePath}\" \n";
-        
-        $routerString .= "x-template". ($routeProp['preload'] ?? false ? ".preload" : "") . ($routeProp['interpolate'] ?? false ? ".interpolate" : "") . "=\"";
-        
-        // Print template
-        if(isset($routeProp['template'])) 
-        {
-            
-            if(is_array($routeProp['template'])){
-                $routerString .= str_replace(['"','\/'], ["'",'/'], json_encode($routeProp['template']));
-            } else {
-                $routerString .= $routeProp['template'];
-            }
-            $routerString .= "\" \n";
-        } else {
-            $cleanedPath = preg_replace('/:([^\/]+)/', '', $routePath); // Hapus :param
-            $cleanedPath = "/" . trim($cleanedPath, '/'); // Hapus trailing slash
-            if($cleanedPath === '') {
-                $cleanedPath = '/home';
-            }
 
-            $routerString .= $cleanedPath . "/template\" \n";
+        // Siapkan nilai template
+        $templateStr = "";
+        $hasParamInTemplate = false;
+
+        if (isset($routeProp['template'])) {
+            if (is_array($routeProp['template'])) {
+                $templateStr = str_replace(['"','\/'], ["'",'/'], json_encode($routeProp['template']));
+                $hasParamInTemplate = preg_match('/:([^\/]+)/', implode('', $routeProp['template']));
+            } else {
+                $templateStr = $routeProp['template'];
+                $hasParamInTemplate = preg_match('/:([^\/]+)/', $templateStr);
+            }
+        } else {
+            // Default template jika tidak disediakan
+            $cleanedPath = "/" . trim(preg_replace('/:([^\/]+)/', '', $routePath), '/');
+            $cleanedPath = $cleanedPath === '/' ? '/home' : $cleanedPath;
+            $templateStr = $cleanedPath . "/template";
         }
-        
-        // Print handpler
-        if(isset($routeProp['handler']))
-        {
+
+        $routerString .= "x-template"
+            . (($routeProp['preload'] ?? false) ? ".preload" : "")
+            . ($hasParamInTemplate ? ".interpolate" : "")
+            . "=\"{$templateStr}\" \n";
+
+        if (isset($routeProp['handler'])) {
             $routerString .= "x-handler=\"" . $routeProp['handler'] . "\"";
         }
-        
+
         $routerString .= "></template>\n\n";
     }
 
