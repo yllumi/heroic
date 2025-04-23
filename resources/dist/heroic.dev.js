0c8ea915bb79a4a183d2e79cdb9bff7848db1df8
Toni Haryanto <toha.samba@gmail.com>
2025-04-23 09:45:29 +0700

    Add compatibility for Pinecone Router 7

--- resources/dist/heroic.dev.js
+++ resources/dist/heroic.dev.js
@@ -5608,12 +5608,9 @@ document.addEventListener("alpine:init", () => {
  * tanpa harus menulis kode yang sama berulang-ulang
  **************************************************************************/
 window.$heroic = function({
-    url = null, 
     title = null,
-    perpage = 5,
-    postUrl = null,
-    postRedirect = null,
-    clearCachePath = null,
+    url = null, 
+    clearCachePath = false,
     meta = {}
     } = {}) {
 
@@ -5622,9 +5619,6 @@ window.$heroic = function({
         config: {
             title,
             url,
-            perpage,
-            postUrl,
-            postRedirect,
             clearCachePath
         },
 
@@ -5645,17 +5639,8 @@ window.$heroic = function({
         // Another custom data set by user
         meta: meta,
 
-        // PaginatedData data properties
-        paginatedData: [],
-
-        // Model properties
-        model: {},
-
-        // Model error messages
-        modelMessage: {},
-
         // Function to initialize the page
-        init() {
+        init() {            
             // Set the page title
             this._setTitle();
 
@@ -5663,31 +5648,28 @@ window.$heroic = function({
                 delete $heroicHelper.cached[this.config.clearCachePath]
             }
 
+            this.loadPage();
+        },
+
+        loadPage(fetchUrl = null) {
+            // Prevent same url fetch many times if already loaded
+            if(fetchUrl == this.config.url) return;
+            
+            this.config.url = fetchUrl ?? this.config.url;
+
             // Initialize page data if requested
             if(this.config.url) {
                 // Use $heroicHelper.cached data if exists
                 if($heroicHelper.cached[this.config.url]) {
-                    // Process for list-type data
-                    if($heroicHelper.cached[this.config.url]?.paginatedData) {
-                        $heroicHelper.cached[this.config.url].paginatedData.forEach(item => {
-                            this.paginatedData.push(item)
-                        })
-                        this.ui.nextPage = $heroicHelper.cached[this.config.url].nextPage
-                        this.ui.loadMore = $heroicHelper.cached[this.config.url].loadMore
-                    } 
-                    // Process for row-type data
-                    else {
-                        this.data = $heroicHelper.cached[this.config.url].data
-                    }
+                    this.data = $heroicHelper.cached[this.config.url]
                 } else {
-                    this._fetchPageData();
+                    this.fetchData();
                 }
             }
-
-            window.scrollTo(0,0)
+            window.scrollTo(0,0);
         },
 
-        _fetchPageData() {
+        fetchData() {
             this.ui.loading = true;
             $heroicHelper.fetch(this.config.url)
             .then(response => {
@@ -5707,10 +5689,6 @@ window.$heroic = function({
             });
         },
 
-        reload() {
-            this._fetchPageData();
-        },
-
         assignResponseData(response, cache = true) {
             this.data = response.data;
     
@@ -5718,73 +5696,11 @@ window.$heroic = function({
                 $heroicHelper.cached[this.config.url] = this.data;
         },
 
-        loadMore() {
-            this._fetchPaginatedData(this.ui.nextPage)
-        },
-
-        _fetchPaginatedData(page) {
-            this.ui.loading = true;
-            $heroicHelper.fetch(this.config.url + `?page=` + page)
-            .then(response => {
-                if(response.response_code == 200) {
-                    // Check if response data is a paginatedData
-                    if(response.paginatedData.length > 0) {
-                        this.ui.nextPage += 1;
-                        response.paginatedData.forEach(item => {
-                            this.paginatedData.push(item)
-                        })
-                    } else {
-                        this.ui.empty = true;
-                        this.ui.nextPage = null;
-                        this.ui.loadMore = false;
-                    }
-                    // Save response data to cache
-                    let cached = {paginatedData: this.paginatedData, nextPage: this.ui.nextPage, loadMore: this.ui.loadMore}
-                    $heroicHelper.cached[this.config.url] = cached;
-                } else {
-                    this.ui.error = true;
-                    this.ui.errorMessage = response.message;
-                }
-            })
-            .catch(error => {
-                this.ui.error = true;
-                console.error('Error fetching page data:', error);
-            })
-            .finally(() => {
-                this.ui.loading = false;
-            });
-        },
-
         _setTitle() {
             if(this.config.title){
                 document.title = this.config.title;
             }
         },
-
-        async submitData(params = {}) {
-            if(params?.confirm) {
-                const confirmedBoolean = await Prompts.confirm(params.confirm);
-                if (!confirmedBoolean) return;
-            }
-
-            this.ui.submitting = true
-            this.modelMessage = {}
-            $heroicHelper.post(this.config.postUrl, this.model)
-            .then(response => {
-                if(response.status == 200) {
-                    if(this.config.postRedirect) {
-                        delete $heroicHelper.cached[this.config.clearCachePath]
-                        window.PineconeRouter.context.redirect(this.config.postRedirect)
-                    } else {
-                        this.model = {}
-                        $heroicHelper.toastr('Data saved', 'success', 'bottom');
-                    }
-                } else {
-                    this.modelMessage = response.data.model_messages
-                }
-            })
-        
-        }
     }
 }
 
--- resources/src/core/heroic.js
+++ resources/src/core/heroic.js
@@ -4,12 +4,9 @@
  * tanpa harus menulis kode yang sama berulang-ulang
  **************************************************************************/
 window.$heroic = function({
-    url = null, 
     title = null,
-    perpage = 5,
-    postUrl = null,
-    postRedirect = null,
-    clearCachePath = null,
+    url = null, 
+    clearCachePath = false,
     meta = {}
     } = {}) {
 
@@ -18,9 +15,6 @@ window.$heroic = function({
         config: {
             title,
             url,
-            perpage,
-            postUrl,
-            postRedirect,
             clearCachePath
         },
 
@@ -41,15 +35,6 @@ window.$heroic = function({
         // Another custom data set by user
         meta: meta,
 
-        // PaginatedData data properties
-        paginatedData: [],
-
-        // Model properties
-        model: {},
-
-        // Model error messages
-        modelMessage: {},
-
         // Function to initialize the page
         init() {
             // Set the page title
@@ -59,31 +44,28 @@ window.$heroic = function({
                 delete $heroicHelper.cached[this.config.clearCachePath]
             }
 
+            this.loadPage();
+        },
+
+        loadPage(fetchUrl = null) {
+            // Prevent same url fetch many times if already loaded
+            if(fetchUrl == this.config.url) return;
+            
+            this.config.url = fetchUrl ?? this.config.url;
+
             // Initialize page data if requested
             if(this.config.url) {
                 // Use $heroicHelper.cached data if exists
                 if($heroicHelper.cached[this.config.url]) {
-                    // Process for list-type data
-                    if($heroicHelper.cached[this.config.url]?.paginatedData) {
-                        $heroicHelper.cached[this.config.url].paginatedData.forEach(item => {
-                            this.paginatedData.push(item)
-                        })
-                        this.ui.nextPage = $heroicHelper.cached[this.config.url].nextPage
-                        this.ui.loadMore = $heroicHelper.cached[this.config.url].loadMore
-                    } 
-                    // Process for row-type data
-                    else {
-                        this.data = $heroicHelper.cached[this.config.url].data
-                    }
+                    this.data = $heroicHelper.cached[this.config.url]
                 } else {
-                    this._fetchPageData();
+                    this.fetchData();
                 }
             }
-
-            window.scrollTo(0,0)
+            window.scrollTo(0,0);
         },
 
-        _fetchPageData() {
+        fetchData() {
             this.ui.loading = true;
             $heroicHelper.fetch(this.config.url)
             .then(response => {
@@ -103,10 +85,6 @@ window.$heroic = function({
             });
         },
 
-        reload() {
-            this._fetchPageData();
-        },
-
         assignResponseData(response, cache = true) {
             this.data = response.data;
     
@@ -114,72 +92,10 @@ window.$heroic = function({
                 $heroicHelper.cached[this.config.url] = this.data;
         },
 
-        loadMore() {
-            this._fetchPaginatedData(this.ui.nextPage)
-        },
-
-        _fetchPaginatedData(page) {
-            this.ui.loading = true;
-            $heroicHelper.fetch(this.config.url + `?page=` + page)
-            .then(response => {
-                if(response.response_code == 200) {
-                    // Check if response data is a paginatedData
-                    if(response.paginatedData.length > 0) {
-                        this.ui.nextPage += 1;
-                        response.paginatedData.forEach(item => {
-                            this.paginatedData.push(item)
-                        })
-                    } else {
-                        this.ui.empty = true;
-                        this.ui.nextPage = null;
-                        this.ui.loadMore = false;
-                    }
-                    // Save response data to cache
-                    let cached = {paginatedData: this.paginatedData, nextPage: this.ui.nextPage, loadMore: this.ui.loadMore}
-                    $heroicHelper.cached[this.config.url] = cached;
-                } else {
-                    this.ui.error = true;
-                    this.ui.errorMessage = response.message;
-                }
-            })
-            .catch(error => {
-                this.ui.error = true;
-                console.error('Error fetching page data:', error);
-            })
-            .finally(() => {
-                this.ui.loading = false;
-            });
-        },
-
         _setTitle() {
             if(this.config.title){
                 document.title = this.config.title;
             }
         },
-
-        async submitData(params = {}) {
-            if(params?.confirm) {
-                const confirmedBoolean = await Prompts.confirm(params.confirm);
-                if (!confirmedBoolean) return;
-            }
-
-            this.ui.submitting = true
-            this.modelMessage = {}
-            $heroicHelper.post(this.config.postUrl, this.model)
-            .then(response => {
-                if(response.status == 200) {
-                    if(this.config.postRedirect) {
-                        delete $heroicHelper.cached[this.config.clearCachePath]
-                        window.PineconeRouter.context.redirect(this.config.postRedirect)
-                    } else {
-                        this.model = {}
-                        $heroicHelper.toastr('Data saved', 'success', 'bottom');
-                    }
-                } else {
-                    this.modelMessage = response.data.model_messages
-                }
-            })
-        
-        }
     }
 }
